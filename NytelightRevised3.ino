d65f23e2c1852b188ac6c138b8b9d4ec607a7239
Lourdes Bond <58485263+LdKwannon@users.noreply.github.com>
2021-11-23 02:10:20 -0500

    Update NytelightRevised3.ino

--- NytelightRevised3.ino
+++ NytelightRevised3.ino
@@ -19,6 +19,39 @@
 //Code will let you know if the temperature/humidity sensor is disconnected or non functional via led.
 //
 //uses FastLED (led library) and dht (temp and humidity sensor) libraries, license info for those will be added here when I find them.
+/*The MIT License (MIT)
+
+Copyright (c) 2013 FastLED
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
+//    FILE: dht.h
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.14
+// PURPOSE: DHT Temperature & Humidity Sensor library for Arduino
+//     URL: http://arduino.cc/playground/Main/DHTLib
+//
+// HISTORY:
+// inspired by DHT11 library
+//
+// Released to the public domain
+//
+
 //Humidity sensor is NOT used in this build, though the option exists.
 //
 //Programmer: Lourdes Bond
